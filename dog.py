66ed751cdcabf0305a55ec0bf4abd7f76f2de0bd
kimiekomi <kimiekomi1@gmail.com>
2022-04-17 12:54:12 -0600

    created dog.py and notes.txt

---
+++ dog.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+debug = True
+trace = True
+
+class Dog:
+    def __init__(self):
+        pass
+
+
+    def eat(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
