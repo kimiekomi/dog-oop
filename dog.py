177c3fc7107b7254b853fbad0c82170f4df2fd9a
kimiekomi <kimiekomi1@gmail.com>
2022-04-17 13:11:25 -0600

    added getter/setter to attributes

--- dog.py
+++ dog.py
@@ -4,12 +4,34 @@ debug = True
 trace = True
 
 class Dog:
-    def __init__(self):
-        pass
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
 
+    @property
+    def name(self):
+        return self.name
 
-    def eat(self):
-        pass
+    @name.setter
+    def name(self, new_name):
+        self.name = new_name
+
+    @property
+    def age(self):
+        return self.age
+
+    @age.setter
+    def name(self, new_age):
+        self.name = new_age
+
+    @property
+    def breed(self):
+        return self.breed
+
+    @breed.setter
+    def name(self, new_breed):
+        self.name = new_breed
 
 
 if __name__ == "__main__":
