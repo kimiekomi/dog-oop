4f55be1ce806f1b3608398c1065bca7aa4744a9b
kimiekomi <kimiekomi1@gmail.com>
2022-04-18 12:41:10 -0600

    moved Timer class to separate file

---
+++ timer.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python3
+
+import time
+
+debug = False
+trace = False
+
+class Timer:
+    def __init__(self):
+        if debug: print("constructed Timer class")
+
+        self.timer_started = False
+
+
+    def start(self):
+        if debug: print("called start()")
+
+        if self.timer_started:
+            print("Timer already started")
+
+        self.timer_started = True
+        self.start_time = time.perf_counter()
+
+        if trace: print(f"Start Time: {self.start_time:0.1f}")
+
+    
+    def check_time(self):
+        if debug: print("called check_time()")
+
+        if not self.timer_started:
+            print("Timer has NOT started")
+
+        self.current_time = time.perf_counter()
+
+        print(f"Current Time: {self.current_time:0.1f}")
+
+        return self.current_time
+
+
+    def stop(self):
+        if debug: print("called stop()")
+
+        if not self.timer_started:
+            print("Timer has NOT started")
+
+        self.stop_time = time.perf_counter()
+        self.timer_started = False
+
+        if trace: print(f"Stop Time: {self.stop_time:0.1f}")
+
+    
+    def elapsed_time(self):
+        if debug: print("called elapsed_time()")
+
+        if not self.timer_started:
+            print("Timer has NOT started")
+
+        self.stop_time = time.perf_counter()
+        self.total_time = self.stop_time - self.start_time
+
+        if trace: print(f"Elapsed Time: {self.elapsed_time:0.1f} seconds")
+
+        return self.total_time
+
+
+if __name__ == "__main__":
+    timer = Timer()
+    time.sleep(2)
+    timer.start()
+    time.sleep(1)
+    timer.stop()
+
+    print()
+    
+    time.sleep(2)
+    timer.start()
+    time.sleep(1)
+    timer.elapsed_time()
+    time.sleep(1)
+    timer.stop()
+
