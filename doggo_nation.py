8601fd48de38608f11992b39245764c6d57cd808
kimiekomi <kimiekomi1@gmail.com>
2022-04-22 15:49:15 -0600

    modified process_input() to simply return a string

--- doggo_nation.py
+++ doggo_nation.py
@@ -29,26 +29,66 @@ class Doggo_Nation():
             print("\n*** Let's Interact with Your Dog! ***")
             
             # select dog activity
-            self.selected_activity = self.input_activity()
+            selected_activity = self.input_activity()
             # process dog activity selections 
-            next_option = self.process_activity(self.selected_activity)
+            next_option = self.process_input(selected_activity)
 
-            if next_option == 0:
+            if next_option == "exit":
+                print("\nGoodbye...See you next time!\n")
                 break
 
-            if next_option == 1:
+            if next_option == "name":
+                new_name = input(f"\nYour dog's current name is {self.active_dog.name}.\nEnter your dog's new name: ")
+
+                new_name = new_name[0].upper() + new_name[1:]
+
+                self.active_dog.name = new_name
+
+                print(f"\n>>> Your dog's name is now {self.active_dog.name}.")
+
+            elif next_option == "age":
+                print(f"\n>>> Your dog is {self.active_dog.age} year(s) old.")
+
+            elif next_option == "breed":
+                print(f"\n>>> Your dog is a(n) {self.active_dog.breed}.")
+
+            elif next_option == "feed":
+                print(f"\nLet's feed {self.active_dog.name} some kibbles!")
+                self.active_dog.eat()
+
+            elif next_option == "walk":
+                print(f"\nLet's take {self.active_dog.name} for a walk!")
+                self.active_dog.walk()
+
+            elif next_option == "potty":
+                print(f"\nLet's take {self.active_dog.name} to potty!")
+                self.active_dog.potty()
+
+            elif next_option == "treat":
+                print(f"\nLet's give {self.active_dog.name} a treat!")
+                self.active_dog.treat()
+
+            elif next_option == "sleep":
+                print(f"\nLet's put {self.active_dog.name} down for a nap!")
+                self.active_dog.sleep()
+
+            elif next_option == "switch":
                 if len(self.dog_entries) > 1:
-                    self.activate_dog()
-                    continue
+                    self.active_dog = self.activate_dog()
 
                 print("There is only one dog available...")
 
-            if next_option == 2:
+            elif next_option == "new":
                 self.create_dog()
                 self.active_dog = self.activate_dog()
-                continue
 
-    
+            elif next_option == "m":
+                self.list_activities(self.active_dog.name)
+
+            else:
+                print(">>> ERROR: unavailable option")
+
+
     def create_dog(self):
         if debug: print("called create_dog()")
 
@@ -164,61 +204,48 @@ class Doggo_Nation():
         return activity
 
 
-    def process_activity(self, split_activity):
-        if debug: print("called process_activity()")
+    def process_input(self, split_input):
+        if debug: print("called process_input()")
 
-        active_dog = self.activate_dog()
 
-        if "name" in split_activity: 
-            new_name = input(f"\nYour dog's current name is {active_dog.name}.\nEnter your dog's new name: ")
+        if "name" in split_input: 
+            return "name"
+            
+        elif "age" in split_input: 
+            return "age"
 
-            new_name = new_name[0].upper() + new_name[1:]
+        elif "breed" in split_input: 
+            return "breed"
 
-            active_dog.name = new_name
+        elif "feed" in split_input: 
+            return "feed"
+            
+        elif "walk" in split_input: 
+            return "walk"
+            
+        elif "potty" in split_input: 
+            return "potty"
+            
+        elif "treat" in split_input: 
+            return "treat"
+            
+        elif "sleep" in split_input: 
+            return "sleep"
 
-            print(f"\n>>> Your dog's name is now {active_dog.name}.")
+        elif "exit" in split_input: 
+            return "exit"
+            
+        elif "switch" in split_input: 
+            return "switch"
 
-        elif "age" in split_activity: 
-            print(f"\n>>> Your dog is {active_dog.age} year(s) old.")
-
-        elif "breed" in split_activity: 
-            print(f"\n>>> Your dog is a(n) {active_dog.breed}.")
-
-        elif "feed" in split_activity: 
-            print(f"\nLet's feed {active_dog.name} some kibbles!")
-            active_dog.eat()
-
-        elif "walk" in split_activity: 
-            print(f"\nLet's take {active_dog.name} for a walk!")
-            active_dog.walk()
-
-        elif "potty" in split_activity: 
-            print(f"\nLet's take {active_dog.name} to potty!")
-            active_dog.potty()
-
-        elif "treat" in split_activity: 
-            print(f"\nLet's give {active_dog.name} a treat!")
-            active_dog.treat()
-
-        elif "sleep" in split_activity: 
-            print(f"\nLet's put {active_dog.name} down for a nap!")
-            active_dog.sleep()
-
-        elif "new" in split_activity: 
-            return 2
+        elif "new" in split_input: 
+            return "new"
         
-        elif "switch" in split_activity: 
-            return 1
-
-        elif "exit" in split_activity: 
-            print("\nGoodbye...See you next time!\n")
-            return 0
-
-        elif "m" in split_activity:
-            self.list_activities(active_dog.name)
+        elif "m" in split_input:
+            return "m"
 
         else: 
-            print(">>> ERROR: That option is not available")
+            return
 
 
 def enter_doggo():
